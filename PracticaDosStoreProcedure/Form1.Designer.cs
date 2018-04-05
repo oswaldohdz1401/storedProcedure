36471ce38e15a350134cb99b6d91e857071d9dc3
Oswaldo Hernández <33918023+oswaldohernandez11@users.noreply.github.com>
2018-04-05 08:31:49 -0500

    C#

---
+++ PracticaDosStoreProcedure/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace PracticaDosStoreProcedure
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBID = new System.Windows.Forms.TextBox();
+            this.textBTelefono = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.bGuardar = new System.Windows.Forms.Button();
+            this.textBNombre = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBID
+            // 
+            this.textBID.Location = new System.Drawing.Point(123, 24);
+            this.textBID.Name = "textBID";
+            this.textBID.Size = new System.Drawing.Size(185, 20);
+            this.textBID.TabIndex = 0;
+            // 
+            // textBTelefono
+            // 
+            this.textBTelefono.Location = new System.Drawing.Point(123, 102);
+            this.textBTelefono.Name = "textBTelefono";
+            this.textBTelefono.Size = new System.Drawing.Size(185, 20);
+            this.textBTelefono.TabIndex = 2;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.BackColor = System.Drawing.Color.Transparent;
+            this.lblID.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblID.Location = new System.Drawing.Point(17, 30);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(33, 23);
+            this.lblID.TabIndex = 3;
+            this.lblID.Text = "ID";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.Color.Transparent;
+            this.lblNombre.Font = new System.Drawing.Font("Verdana", 14.25F);
+            this.lblNombre.Location = new System.Drawing.Point(17, 64);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(85, 23);
+            this.lblNombre.TabIndex = 4;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.BackColor = System.Drawing.Color.Transparent;
+            this.lblTelefono.Font = new System.Drawing.Font("Verdana", 14.25F);
+            this.lblTelefono.Location = new System.Drawing.Point(17, 105);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(91, 23);
+            this.lblTelefono.TabIndex = 5;
+            this.lblTelefono.Text = "Telefono";
+            // 
+            // bGuardar
+            // 
+            this.bGuardar.Location = new System.Drawing.Point(194, 143);
+            this.bGuardar.Name = "bGuardar";
+            this.bGuardar.Size = new System.Drawing.Size(114, 43);
+            this.bGuardar.TabIndex = 6;
+            this.bGuardar.Text = "Guardar";
+            this.bGuardar.UseVisualStyleBackColor = true;
+            // 
+            // textBNombre
+            // 
+            this.textBNombre.Location = new System.Drawing.Point(123, 61);
+            this.textBNombre.Name = "textBNombre";
+            this.textBNombre.Size = new System.Drawing.Size(185, 20);
+            this.textBNombre.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nombre,
+            this.Telefono});
+            this.dataGridView1.Location = new System.Drawing.Point(326, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(344, 138);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Telefono
+            // 
+            this.Telefono.HeaderText = "Telefono";
+            this.Telefono.Name = "Telefono";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(735, 206);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBNombre);
+            this.Controls.Add(this.bGuardar);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.textBTelefono);
+            this.Controls.Add(this.textBID);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Practica Dos Stored Procedure";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBID;
+        private System.Windows.Forms.TextBox textBTelefono;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Button bGuardar;
+        private System.Windows.Forms.TextBox textBNombre;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefono;
+    }
+}
+
